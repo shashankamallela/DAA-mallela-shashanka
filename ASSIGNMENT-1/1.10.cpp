23442c74711b6a76429fbec35145d1caf2769e16
shashankamallela <152372641+shashankamallela@users.noreply.github.com>
2024-06-03 22:33:57 +0530

    Add files via upload

---
+++ ASSIGNMENT-1/1.10.cpp
@@ -0,0 +1,55 @@
+// A recursive C# program to 
+// check whether a given number
+// is palindrome or not
+using System;
+
+class GFG
+{
+
+	// A recursive function that 
+	// check a str(s..e)
+	// is palindrome or not.
+	static bool isPalRec(String str, 
+						int s, 
+						int e)
+	{
+		
+		// If there is only one character
+		if (s == e)
+			return true;
+
+		// If first and last character
+		// do not match
+		if ((str[s]) != (str[e]))
+			return false;
+
+		// If there are more than two
+		// characters, check if middle
+		// substring is also
+		// palindrome or not.
+		if (s < e + 1)
+			return isPalRec(str, s + 1, 
+							e - 1);
+			return true;
+	}
+
+	static bool isPalindrome(String str)
+	{
+		int n = str.Length;
+
+		if (n == 0)
+			return true;
+
+		return isPalRec(str, 0, n - 1);
+	}
+	public static void Main()
+	{
+		String str = "geeg";
+
+		if (isPalindrome(str))
+			Console.Write("Yes");
+		else
+			Console.Write("No");
+	}
+}
+
